ca2fc7d96ba6d346ebb2819c3639dc57cd31b4af
Anthony Sena <asena5@its.jnj.com>
2020-09-03 22:44:09 -0400

    Poster updates

--- extras/Plots/GenerateStackedAreaChart.R
+++ extras/Plots/GenerateStackedAreaChart.R
@@ -37,7 +37,7 @@ write.csv(studySummary, file="studySummary.csv")
 sql <- "select database, year, step_1, SUM(personcount) personcount
         from public.network_pathways_results
         where database NOT IN ('Hospital', 'Germany')
-        group by database, year, step_1 HAVING SUM(personcount) > 5
+        group by database, year, step_1 --HAVING SUM(personcount) > 5
         order by database, year, step_1
         ;"
 
@@ -45,7 +45,7 @@ sql <- "select database, year, step_1, SUM(personcount) personcount
 results <- DatabaseConnector::querySql(connection = srDbConnection, sql = sql)
 on.exit(DatabaseConnector::disconnect(connection))
 
-# Format the results
+# write.csv(data, "dusByDBAndYear.csv", row.names = FALSE)
 
 
 # Create a friendly group name to use that cleans "[EHDEN RA] <drug> use" into "<drug>"
@@ -62,13 +62,150 @@ dbSummary <- results %>%
 
 
 # Write all results to CSV
-# data <- results  %>%
-#   group_by(DATABASE, YEAR, group) %>%
-#   summarise(n = sum(PERSONCOUNT)) %>%
-#   mutate(percentage = n / sum(n)) %>%
-#   arrange(desc(DATABASE, group))
+data <- results  %>%
+  group_by(DATABASE, YEAR, group) %>%
+  summarise(n = sum(PERSONCOUNT)) %>%
+  mutate(percentage = n / sum(n)) %>%
+  arrange(desc(DATABASE, group))
+
+# Get the totals
+countByDB <- aggregate(data$n, by=list(Database=data$DATABASE), FUN=sum)
+totalN <- sum(data$n)
+
+formatDbName <- function(fullName, dbKey, countByDB) {
+  return(paste0(fullName, "\n(n=", prettyNum(countByDB[countByDB$Database == dbKey, ]$x, big.mark=","), ")"))
+}
+
+#format database
+named_databases <- c(
+  formatDbName("IQVIA IMS - EMR, AU", "Australia", countByDB),
+  formatDbName("IQVIA LPD - EMR, BE", "Belgium", countByDB),
+  #  formatDbName("IQVIA IMS - EMR, DE", "Germany", countByDB),
+  formatDbName("Estonia - EMR, EE", "Estonia", countByDB),
+  formatDbName("SIDIAP - EMR, ES", "SIDIAP", countByDB),
+  formatDbName("IQVIA IMS - EMR, FR", "France", countByDB),
+  formatDbName("IQVIA LPD - Other, FR", "France", countByDB),
+  formatDbName("JMDC - Claims, JP", "JMDC", countByDB),
+  formatDbName("IPCI - EMR, NL", "IPCI", countByDB),
+  formatDbName("IQVIA THIN - EMR, UK", "THIN", countByDB),
+  formatDbName("IBM CCAE - Claims, US", "CCAE", countByDB),
+  formatDbName("IBM MDCD - Claims, US", "MDCD", countByDB),
+  formatDbName("IBM MDCR - Claims, US", "MDCR", countByDB),
+  formatDbName("IQVIA Amb - EMR, US", "AmbEMR", countByDB),
+  #formatDbName("IQVIA HUCM - Other, US", "Hospital", countByDB),
+  formatDbName("OPTUM DOD - Claims, US", "Optum_DOD", countByDB),
+  formatDbName("OPTUM EHR - EMR, US", "Optum_Panther", countByDB)
+)
+
+
+data$DB_KEY <- data$DATABASE
+data$DATABASE[data$DATABASE == "AmbEMR"] <- formatDbName("IQVIA Amb - EMR, US", "AmbEMR", countByDB)
+data$DATABASE[data$DATABASE == "Australia"] <- formatDbName("IQVIA IMS - EMR, AU", "Australia", countByDB)
+data$DATABASE[data$DATABASE == "Belgium"] <- formatDbName("IQVIA LPD - EMR, BE", "Belgium", countByDB)
+data$DATABASE[data$DATABASE == "CCAE"] <- formatDbName("IBM CCAE - Claims, US", "CCAE", countByDB)
+data$DATABASE[data$DATABASE == "Estonia"] <- formatDbName("Estonia - EMR, EE", "Estonia", countByDB)
+data$DATABASE[data$DATABASE == "France"] <- formatDbName("IQVIA IMS - EMR, FR", "France", countByDB)
+data$DATABASE[data$DATABASE == "IPCI"] <- formatDbName("IPCI - EMR, NL", "IPCI", countByDB)
+data$DATABASE[data$DATABASE == "JMDC"] <- formatDbName("JMDC - Claims, JP", "JMDC", countByDB)
+data$DATABASE[data$DATABASE == "MDCD"] <- formatDbName("IBM MDCD - Claims, US", "MDCD", countByDB)
+data$DATABASE[data$DATABASE == "MDCR"] <- formatDbName("IBM MDCR - Claims, US", "MDCR", countByDB)
+data$DATABASE[data$DATABASE == "Optum_DOD"] <- formatDbName("OPTUM DOD - Claims, US", "Optum_DOD", countByDB)
+data$DATABASE[data$DATABASE == "Optum_Panther"] <- formatDbName("OPTUM EHR - EMR, US", "Optum_Panther", countByDB)
+data$DATABASE[data$DATABASE == "SIDIAP"] <- formatDbName("SIDIAP - EMR, ES", "SIDIAP", countByDB)
+data$DATABASE[data$DATABASE == "THIN"] <- formatDbName("IQVIA THIN - EMR, UK", "THIN", countByDB)
+
+# IPCI has some strange data in 2000-2001 so I'm taking that out
+data <- data[!(data$DB_KEY == "IPCI" & data$YEAR < 2002), ]
+
+
+# data %>% group_by(DATABASE) %>% summarise(n = sum(n)) %>% arrange(DATABASE)
+# sum(data$n)
+
+# # Revise database names for poster
+# data$DATABASE_FORMATTED <- data$DATABASE
+# data$DATABASE_FORMATTED[data$DATABASE == "Optum_Panther"] <- "Optum EHR\n(n = 123)"
+# data$DATABASE_FORMATTED[data$DATABASE == "Optum_DOD"] <- "Optum DOD"
+# data$DATABASE_FORMATTED[data$DATABASE == "CCAE"] <- "IBM CCAE"
+# data$DATABASE_FORMATTED[data$DATABASE == "MDCD"] <- "IBM MDCD"
+# data$DATABASE_FORMATTED[data$DATABASE == "MDCR"] <- "IBM MDCR"
+# data$DATABASE_FORMATTED[data$DATABASE == "THIN"] <- "THIN (UK)"
+# data$DATABASE_FORMATTED[data$DATABASE == "SIDIAP"] <- "SIDIAP (ES)"
+# data$DATABASE_FORMATTED[data$DATABASE == "IPCI"] <- "IPCI (NL)"
+# data$DATABASE_FORMATTED[data$DATABASE == "JMDC"] <- "JMDC (JP)\n(n=12345)"
+
+# Further summarize by grouping into "other" bucket for those that are
+# not the main ingredients of interest
+allTreatments <- unique(data$group)
+treatmentsForSecularTrends <- c("methotrexate", "sulfasalazine", "leflunomide", "hydroxychloroquine")
+otherDrugsRolledUp <- data[!data$group %in% treatmentsForSecularTrends,] %>%
+  group_by(DATABASE, DB_KEY, YEAR) %>% 
+  summarise(group = "Other", n = sum(n), percentage = sum(percentage))
+dataForStackedBar <- rbind(data[data$group %in% treatmentsForSecularTrends, ], 
+                           otherDrugsRolledUp)
+my.levels <- c("methotrexate", "sulfasalazine", "leflunomide", "hydroxychloroquine", "Other")
+dataForStackedBar <- dataForStackedBar %>%
+  arrange(desc(DATABASE), YEAR, factor(group, my.levels))
+dataForStackedBar$group <- factor(dataForStackedBar$group, levels = rev(unique(dataForStackedBar$group)))
+
+
+#For plotting
+stackedBarChart <- function(d, rows, cols, show.legend=F) {
+  p <- ggplot(d, aes(fill=d$group, y=d$percentage, x=d$YEAR)) + 
+    geom_bar(position="fill", stat="identity", show.legend = show.legend) +
+    scale_fill_brewer(palette = "Spectral", direction=1) +
+    labs(x = "Year", y = "Percentage (%)") +
+    scale_y_continuous(labels=scales::percent) +
+    guides(fill=guide_legend(title="Treatment")) #+
+    #theme(legend.position = "bottom")
+
+  p <- p + facet_wrap(facets = vars(d$DATABASE),
+                      nrow=rows,
+                      ncol=cols)
+  return(p)
+}
+
+# Now facet on the DBs
+allDbs <- unique(data$DATABASE)
+usaDbs <- c("CCAE", "MDCR", "MDCD", "AmbEMR", "Optum_Panther", "Optum_DOD")
+eurDbs <- c("THIN", "SIDIAP", "IPCI", "France", "Estonia", "Belgium")
+apDbs <- c("JMDC", "Australia")
+usaPlot <- stackedBarChart(dataForStackedBar[dataForStackedBar$DB_KEY %in% usaDbs,], 2, 3, F)
+eurPlot <- stackedBarChart(dataForStackedBar[dataForStackedBar$DB_KEY %in% eurDbs,], 2, 3, F)
+apPlot <- stackedBarChart(dataForStackedBar[dataForStackedBar$DB_KEY %in% apDbs,], 1, 3, T)
+gridExtra::grid.arrange(usaPlot, eurPlot, apPlot)
+
+# Try facet grid vs wrap
+dataForStackedBar$countryGroup <- dataForStackedBar$DB_KEY
+dataForStackedBar$countryGroup[dataForStackedBar$DB_KEY %in%usaDbs] <- "United States"
+dataForStackedBar$countryGroup[dataForStackedBar$DB_KEY %in%eurDbs] <- "Europe"
+dataForStackedBar$countryGroup[dataForStackedBar$DB_KEY %in%apDbs] <- "Asia Pacific"
+
+
+p <- ggplot(dataForStackedBar, aes(fill=dataForStackedBar$group, y=dataForStackedBar$percentage, x=dataForStackedBar$YEAR)) +
+  geom_bar(position="fill", stat="identity") +
+  scale_fill_brewer(palette = "Spectral", direction=1) +
+  labs(x = "Year", y = "Percentage (%)") +
+  scale_y_continuous(labels=scales::percent) +
+  guides(fill=guide_legend(title="Treatment")) +
+  theme(legend.position = "bottom")
+p + facet_grid(dataForStackedBar$countryGroup ~ dataForStackedBar$DATABASE)
+# p + facet_wrap(facets = vars(d$DATABASE),
+#                nrow=rows,
+#                ncol=cols)
+
+
 # 
-# write.csv(data, "dusByDBAndYear.csv", row.names = FALSE)
+# 
+# my.levels <- c("methotrexate", "sulfasalazine", "leflunomide", "hydroxychloroquine", "Other")
+# thinData <- dataForStackedBar[dataForStackedBar$DATABASE == 'THIN',] %>%
+#   arrange(desc(DATABASE), YEAR, factor(group, my.levels))
+# thinData$group <- factor(thinData$group, levels = rev(unique(thinData$group)))
+# #drugLevels$color <- factor(drugLevels$color, levels = c("#CA0020", "#F4A582", "#DFDFDF", "#DFDFDF", "#92C5DE", "#0571B0"), ordered=T)
+# ggplot(thinData, aes(fill=thinData$group, y=thinData$percentage, x=thinData$YEAR)) + 
+#   geom_bar(position="fill", stat="identity") +
+#   labs(x = "year", y = "percentage (%)")
+# #  scale_fill_identity("Percent", labels = my.levels)
+
 
 # Loop through each DB, create the area plot and save it to the output folder
 studyPeriod = c(2008:2018)
@@ -113,7 +250,7 @@ for (database in dbList) {
     guides(color=guide_legend(nrow=2)) +
     scale_fill_brewer(palette = "Spectral", direction = 1)
   
-  plot(fig)
+  #plot(fig)
   
   ggsave(paste0(outputFolder, "/", database, ".jpg"), fig,  width = 10, height = 9, dpi = 300, units = "in", device='jpg')
 }
